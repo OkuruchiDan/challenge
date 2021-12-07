e8ba89419814a2540c77d88e944eb78de92f964f
OkuruchiDan <bogdan.skrypnyk@perfsol.tech>
2021-12-07 15:05:33 +0200

    chaned declaration of example.json file

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,24 @@ import '../styles/globals.css'
 import App from 'next/app'
 
 function MyApp({ Component, pageProps, dataFromApp, exampleJson,query }) {
-  return <Component {...pageProps} dataFromApp={dataFromApp.long_tails} exampleJson={exampleJson} query={query} />
+  return <Component {...pageProps} dataFromApp={dataFromApp} exampleJson={exampleJson} query={query} />
 }
 
 MyApp.getInitialProps = async (appContext) => {
   const appProps = await App.getInitialProps(appContext);
-  const res = await fetch(process.env.HASURA_API_URL + '/long_tails', {
+  const res = await fetch(process.env.HASURA_API_URL + '/all_data', {
     headers: {
       'content-type': 'application/json',
       'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET
     }
   })
   const data = await res.json()
-  const exampleJson = require('../example.json')
-  return { ...appProps, dataFromApp: data, exampleJson, query: appContext.router.query }
+  return {
+    ...appProps,
+    dataFromApp: data.long_tails,
+    exampleJson: data.exampleJson.example,
+    query: appContext.router.query
+  }
 }
 
 export default MyApp
