f523e742ec0bc1561f2c22542de0302468d518e6
OkuruchiDan <bogdan.skrypnyk@perfsol.tech>
2021-12-07 15:12:38 +0200

    added try catch block

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,28 @@
 import '../styles/globals.css'
 import App from 'next/app'
 
-function MyApp({ Component, pageProps, dataFromApp, exampleJson,query }) {
-  return <Component {...pageProps} dataFromApp={dataFromApp} exampleJson={exampleJson} query={query} />
+function MyApp({ Component, pageProps, dataFromApp, exampleJson,query, error }) {
+  return <Component {...pageProps} dataFromApp={dataFromApp} exampleJson={exampleJson} query={query} error={error} />
 }
 
 MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
-  const res = await fetch(process.env.HASURA_API_URL + '/all_data', {
-    headers: {
-      'content-type': 'application/json',
-      'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET
+  try {
+    const appProps = await App.getInitialProps(appContext);
+    const res = await fetch(process.env.HASURA_API_URL + '/all_data', {
+      headers: {
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET
+      }
+    })
+    const data = await res.json()
+    return {
+      ...appProps,
+      dataFromApp: data.long_tails,
+      exampleJson: data.exampleJson.example,
+      query: appContext.router.query
     }
-  })
-  const data = await res.json()
-  return {
-    ...appProps,
-    dataFromApp: data.long_tails,
-    exampleJson: data.exampleJson.example,
-    query: appContext.router.query
+  } catch (e) {
+    return { error: e }
   }
 }
 
